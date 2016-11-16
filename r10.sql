1a2213b0313628e3adc82b500396334c457bb626
DecadeWoW <decadewow2016@gmail.com>
2016-11-16 15:05:18 +0800

    Update r10.sql

--- r10.sql
+++ r10.sql
@@ -5,8 +5,8 @@ UPDATE item_template SET requiredhonorrank = 6  WHERE requiredhonorrank = 16
 UPDATE item_template SET requiredhonorrank = 7  WHERE requiredhonorrank = 17
 
 #bg
-UPDATE battleground_template SET MinPlayersPerTeam = 0
+UPDATE battleground_template SET MinPlayersPerTeam = 1
 
 #gm command
 UPDATE command SET SECURITY = 2 WHERE SECURITY = 1
-UPDATE command SET SECURITY = 1 WHERE NAME IN ('name','additem','announce','gm','go','goname','maxskill')
+UPDATE command SET SECURITY = 1 WHERE NAME IN ('name','additem','announce','gm','go','goname','maxskill','revive','namego','kick','levelup')
