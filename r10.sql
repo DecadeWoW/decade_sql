fb4dad63eb48ac46ba829718515e6fcb819ca4ae
DecadeWoW <decadewow2016@gmail.com>
2016-12-19 09:38:16 +0800

    fix battleground medical

--- r10.sql
+++ r10.sql
@@ -10,3 +10,7 @@ UPDATE battleground_template SET MinPlayersPerTeam = 1;
 #gm command
 UPDATE command SET SECURITY = 2 WHERE SECURITY = 1;
 UPDATE command SET SECURITY = 1 WHERE NAME IN ('additem','announce','gm','go','goname','maxskill','revive','kick','namego','modify money','levelup','learn');
+
+##bg medical
+UPDATE creature_template SET FactionAlliance = 35, FactionHorde = 35 WHERE Entry = 13116;
+UPDATE creature_template SET FactionAlliance = 35, FactionHorde = 35 WHERE Entry = 13117;
